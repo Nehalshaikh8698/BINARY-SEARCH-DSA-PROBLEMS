9d03c3771971edd2ea2c1e2211b4de645a081a19
Nehalshaikh8698 <nehalshaikh02017@gmail.com>
2025-11-28 18:51:55 +0530

    code

--- leetcode_35.cpp
+++ leetcode_35.cpp
@@ -30,3 +30,23 @@
 // -104 <= nums[i] <= 104
 // nums contains distinct values sorted in ascending order.
 // -104 <= target <= 104
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return left; // insertion point
+    }
+};
