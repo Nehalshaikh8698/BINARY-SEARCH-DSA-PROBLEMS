505474721e984bb07a4783499f1e14d29b789e24
Nehalshaikh8698 <nehalshaikh02017@gmail.com>
2025-11-28 19:00:43 +0530

    final code

--- leetcode_367.cpp
+++ leetcode_367.cpp
@@ -26,3 +26,26 @@
 // Constraints:
 
 // 1 <= num <= 231 - 1
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num < 1) return false;
+
+        long long left = 1, right = num;  // use long long to avoid overflow
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long sq = mid * mid;
+
+            if (sq == num)
+                return true;
+            else if (sq < num)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return false;
+    }
+};
